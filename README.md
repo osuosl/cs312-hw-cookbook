3a3c0015f5076824ed27d3e32e3257892137af53
Jordan Evans <jordane@osuosl.org>
2015-02-11 10:45:26 -0800

    commit initial hw cb

---
+++ README.md
@@ -0,0 +1,4 @@
+# cs312-hw-cookbook-cookbook
+
+In this cookbook you will find a set of recipes and tests. The tests currently
+do not pass. Your job is to finish the recipes so that they pass all tests.
---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,24 @@
+require 'serverspec'
+
+set :background, :exec
+
+%w(vim emacs zsh ksh).each do |p|
+  describe package(p) do
+    it { should be_installed }
+  end
+end
+
+describe file('/home/centos/.bashrc') do
+  it { should be_file }
+  its(:content) { should match %r{PATH=$PATH:/opt/chef/bin} }
+  its(:content) { should match %r{CDPATH=/home/centos} }
+end
+
+describe service('httpd') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe iptables do
+  it { should have_rule('-A INPUT -p tcp -m tcp --dport 22 -j ACCEPT') }
+end
