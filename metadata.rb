f747a212774a8b504e2c574f3a39d1f80b895c98
Jordan Evans <jordane@osuosl.org>
2015-03-03 09:35:22 -0800

    correct licensing info

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cs312-hw-cookbook'
 maintainer       'OSU Open Source Lab'
 maintainer_email 'chef@osuosl.org'
-license          'All rights reserved'
+license          'Apache v2'
 description      'Installs/Configures cs312-hw-cookbook'
 long_description 'Installs/Configures cs312-hw-cookbook'
 version          '0.0.1'
